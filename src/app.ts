ab4e21c8b045f9ed93445f66c788d585eadca31e
Mateusz Klencner <klencner97@gmail.com>
2024-02-29 11:19:41 +0100

    Add init app (index.ts)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
 import {app} from './server.ts'
 
+// import { PrismaClient } from '@prisma/client'
 
+interface IConfig {
+    env: string
+    port: number
+}
+
+// const prisma = new PrismaClient()
+
+export function initApp(config: IConfig) {
+    // startWebsocketServer(config.port)
+    
+    app.listen(config.port, (listenSocket) => {
+        if(listenSocket !== false) {
+            console.log(`Server is running on http://localhost:${config.port}`)
+        } else {
+            console.log('Error: Server running problem.')
+        }
+    })
+}
+
+//
